d60030e49b32225b6487f934caefbbca79ab01a9
Sunday Mebawondu <akinbode4life.com>
2024-04-25 03:15:21 -0700

    install puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# installs flask from pip using puppet
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
