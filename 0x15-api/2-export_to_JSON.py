e3ff4188e85fe91ae7a36f67ce8324d308d18d38
Sunday Mebawondu <akinbode4life.com>
2024-05-20 19:13:29 -0700

    0x15

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-"""
-   Module that access API of user's todo list
-"""
+""" Python to get data from an API and convert to Json"""
+import csv
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    employee_ID = int(sys.argv[1])
-    base_url = 'https://jsonplaceholder.typicode.com'
-    users_url = '{}/users'.format(base_url)
-    users_response = requests.get(users_url)
-    users_data = users_response.json()
 
-    matching_user = None
-    for employee in users_data:
-        if employee['id'] == employee_ID:
-            matching_user = employee
-            break
-    if matching_user:
-        employee_name = matching_user['name']
-        todo_url = f'{base_url}/todos?userId={employee_ID}'
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
-        completed = []
-        for todo in todo_data:
-            if todo['completed']:
-                completed.append(todo)
-        json_data = {str(employee_ID): []}
-        for task in todo_data:
-            json_data[str(employee_ID)].append({
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": employee_name})
-        json_filename = "{}.json".format(employee_ID)
-        with open(json_filename, "w") as file:
-            json.dump(json_data, file)
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    res = requests.get(url_to_user)
+    """Documentation"""
+    USERNAME = res.json().get('username')
+    """Documentation"""
+    url_to_task = url_to_user + '/todos'
+    res = requests.get(url_to_task)
+    tasks = res.json()
+
+    dict_data = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        dict_data[USER_ID].append({
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+    """print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+        json.dump(dict_data, f)
