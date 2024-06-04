b433fca883125c825aac1d5d35eba00388bb5d6a
Sunday Mebawondu <akinbode4life.com>
2024-06-04 13:12:43 -0700

    update 0x16

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""Module that scrapes reddit api"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-       Scrapes reddit api and gets the number of subscribers
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    header = {'User-Agent': 'Get subscribers info'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    data = requests.get(url, headers=header, allow_redirects=False)
-
-    if data.status_code == 200:
-        try:
-            data = data.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (KeyError, ValueError):
-            return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
