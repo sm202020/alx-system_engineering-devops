3788ae5e2b3fed7d4e701ee86e60c1a1886c8fe6
Sunday Mebawondu <akinbode4life@gmail.com>
2024-08-20 22:08:08 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,17 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  
-        results = response.json().get("data")
-        return results.get("subscribers", 0)  
-    except requests.exceptions.HTTPError:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
